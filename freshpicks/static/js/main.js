1528c0adf80a36422ba252efdc95d309d23cdeae
mukuru-bless <blessing.chiura@mukuru.com>
2020-09-23 17:17:25 +0200

    update min width

--- freshpicks/static/js/main.js
+++ freshpicks/static/js/main.js
@@ -72,7 +72,7 @@
 	    animateOut: 'fadeOut',
 	    animateIn: 'fadeIn',
 	    nav:false,
-	    autoplayHoverPause: false,
+	    autoplayHoverPause: true,
 	    items: 1,
 	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
 	    responsive:{
